329636191c3437b22439b0d636d3f3b8eaf77c58
Creativa-Server <Creativa-Server@CREATIVASERVER>
2018-05-04 12:22:06 -0500

    Login

--- CampaniaElectoral/Global.asax.cs
+++ CampaniaElectoral/Global.asax.cs
@@ -24,57 +24,57 @@ namespace CampaniaElectoral
             IPrincipal usr = HttpContext.Current.User;
             if (usr.Identity.IsAuthenticated && usr.Identity.AuthenticationType == "Forms")
             {
-                var httpApp = (HttpApplication)sender;
-                string URL = httpApp.Request.FilePath;
-                string[] URLS = System.Text.RegularExpressions.Regex.Split(URL, "/");
-                HttpContext context = HttpContext.Current;
-                if (httpApp.Request.RawUrl.Split('/').Length > 1)
-                {
-                    string URLValida = URLS[1];
-                    if (URLValida == "Login.aspx")
-                    {
+                //var httpApp = (HttpApplication)sender;
+                //string URL = httpApp.Request.FilePath;
+                //string[] URLS = System.Text.RegularExpressions.Regex.Split(URL, "/");
+                //HttpContext context = HttpContext.Current;
+                //if (httpApp.Request.RawUrl.Split('/').Length > 1)
+                //{
+                //    string URLValida = URLS[1];
+                //    if (URLValida == "Login.aspx")
+                //    {
 
-                    }
-                    else
-                    {
-                        if (context != null && context.Session != null)
-                        {
-                            WN_Usuario u = (WN_Usuario)Session["Usuario"];
-                            if (u == null)
-                            {
+                //    }
+                //    else
+                //    {
+                //        if (context != null && context.Session != null)
+                //        {
+                //            WN_Usuario u = (WN_Usuario)Session["Usuario"];
+                //            if (u == null)
+                //            {
 
-                            }
-                            else
-                            {
-                                if (u.ModulosPadres.Exists(x => x.FrmModulo == URLValida))
-                                {
+                //            }
+                //            else
+                //            {
+                //                if (u.ModulosPadres.Exists(x => x.FrmModulo == URLValida))
+                //                {
 
-                                }
-                                else if (u.ModulosHijos.Exists(x => x.FrmModulo == URLValida))
-                                {
+                //                }
+                //                else if (u.ModulosHijos.Exists(x => x.FrmModulo == URLValida))
+                //                {
 
-                                }
-                                else if (u.ModuloNietos.Exists(x => x.FrmModulo == URLValida))
-                                {
+                //                }
+                //                else if (u.ModuloNietos.Exists(x => x.FrmModulo == URLValida))
+                //                {
 
-                                }
-                                else
-                                {
-                                    if (URLValida == "frmEjemplo.aspx")
-                                    {
+                //                }
+                //                else
+                //                {
+                //                    if (URLValida == "frmEjemplo.aspx")
+                //                    {
 
-                                    }
-                                    else
-                                    {
-                                        FormsAuthentication.SignOut();
-                                        Response.Redirect("/Login.aspx", true);
-                                    }
+                //                    }
+                //                    else
+                //                    {
+                //                        FormsAuthentication.SignOut();
+                //                        Response.Redirect("/Login.aspx", true);
+                //                    }
 
-                                }
-                            }
-                        }
-                    }
-                }
+                //                }
+                //            }
+                //        }
+                //    }
+                //}
             }
         }
     }
