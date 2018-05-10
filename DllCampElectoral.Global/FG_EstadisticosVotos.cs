c1ed0d57f98d133c6a108fcf0c741107c29782ef
CreativaSL <CreativaSL@CREATIVASL-PC>
2018-05-10 17:20:18 -0500

    estadisticas voto

---
+++ DllCampElectoral.Global/FG_EstadisticosVotos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllCampElectoral.Global
+{
+    public class FG_EstadisticosVotos
+    {
+        public int MetaVotosGeneral { get; set; }
+        public int TotalVotosRealizados { get; set; }
+        private int TotalVotosFaltantes { get; set; }
+        private double PorcentajeAvanceGeneralVotos { get; set; }
+
+        public string Conexion { get; set; }
+
+        #region Metodos
+        public int ObtenerTotalVotosFaltantes()
+        {
+            TotalVotosFaltantes = MetaVotosGeneral - TotalVotosFaltantes;
+            return TotalVotosFaltantes;
+        }
+
+        public double CalcularPorcentajeAvanceGeneralVotos()
+        {
+            PorcentajeAvanceGeneralVotos = (TotalVotosRealizados * 100) / MetaVotosGeneral;
+            return PorcentajeAvanceGeneralVotos;
+        }
+
+
+        #endregion
+    }
+}
---
+++ DllCampElectoral.Global/FG_EstadisticosVotos_MetaXHora.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DllCampElectoral.Global
+{
+    public class FG_EstadisticosVotos_MetaXHora
+    {
+        public TimeSpan Hora { get; set; }
+        public decimal Meta { get; set; }
+        public string ColorEtiqueta { get; set; }
+    }
+}
